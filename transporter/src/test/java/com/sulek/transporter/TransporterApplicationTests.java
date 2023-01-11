506844c3a3e1e0f0fd1dcec625b0c2dbbc738a0c
Anıl Şülekoğlu <39844117+sulek1201@users.noreply.github.com>
2023-01-11 20:09:06 +0300

    İnitiated Transporter project

---
+++ transporter/src/test/java/com/sulek/transporter/TransporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sulek.transporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
